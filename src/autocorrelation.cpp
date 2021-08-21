9cfe330502a92f66a11c8f1d7152f2647c1b16a4
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-21 10:48:39 +1000

    Autocorr now works

--- src/autocorrelation.cpp
+++ src/autocorrelation.cpp
@@ -15,6 +15,35 @@ using namespace Rcpp;
 double ac_1(NumericVector x){
 
   int n = x.size();
+  double total = 0;
+  double autocovariance = 0;
+  double variance = 0;
+  double mean, acf;
+
+  // Compute mean
+
+  for (int i = 0; i < n; ++i) {
+    total += x[i];
+  }
+
+  mean = total / n;
+
+  // Compute lag covariance
+
+  for (int i = 0; i < n-1; i++) {
+    autocovariance += (x[i]-mean)*(x[i+1]-mean);
+  }
+
+  // Compute variance
+
+  for (int i = 0; i < n; i++) {
+    variance += (x[i]-mean)*(x[i]-mean);
+  }
+
+  // Return autocorrelation function
+
+  acf = autocovariance/variance;
+  return acf;
 }
 
 //' Calculate autocorrelation function at lag 2
@@ -31,4 +60,33 @@ double ac_1(NumericVector x){
 double ac_2(NumericVector x){
 
   int n = x.size();
+  double total = 0;
+  double autocovariance = 0;
+  double variance = 0;
+  double mean, acf;
+
+  // Compute mean
+
+  for (int i = 0; i < n; ++i) {
+    total += x[i];
+  }
+
+  mean = total / n;
+
+  // Compute lag covariance
+
+  for (int i = 0; i < n-2; i++) {
+    autocovariance += (x[i]-mean)*(x[i+2]-mean);
+  }
+
+  // Compute variance
+
+  for (int i = 0; i < n; i++) {
+    variance += (x[i]-mean)*(x[i]-mean);
+  }
+
+  // Return autocorrelation function
+
+  acf = autocovariance/variance;
+  return acf;
 }
