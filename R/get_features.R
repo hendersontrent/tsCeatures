79da5b9ab5ab69d9d6f14c4cb3f8b8778046ef39
TRENT HENDERSON <trent.henderson1@outlook.com>
2021-08-23 21:53:46 +1000

    Docs

--- R/get_features.R
+++ R/get_features.R
@@ -1,7 +1,7 @@
 #' Automatically run every time-series feature calculation in the package
 #'
 #' @importFrom stats median sd
-#' @param x a numerical time-series input vector
+#' @param x a numerical time-series vector
 #' @return object of class DataFrame that contains the summary statistics for each feature
 #' @author Trent Henderson
 #' @export
@@ -19,6 +19,8 @@ get_features <- function(x){
     outData <- data.frame(mean = mean(x, na.rm = TRUE),
                           median = stats::median(x, na.rm = TRUE),
                           sd = stats::sd(x, na.rm = TRUE),
+                          min = min(x, na.rm = TRUE),
+                          max = max(x, na.rm = TRUE),
                           ac_1 = ac_1(x),
                           ac_2 = ac_2(x),
                           crossing_points = crossing_points(x),
@@ -29,7 +31,8 @@ get_features <- function(x){
                           quantile_75 = quantile_75(x),
                           quantile_90 = quantile_90(x),
                           quantile_95 = quantile_95(x),
-                          prop_above_mean = prop_above_mean(x))
+                          prop_above_mean = prop_above_mean(x),
+                          walker_propcross = walker_propcross(x))
 
     return(outData)
   }
